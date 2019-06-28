465f2a61550a38dcdb66cf041eb246e34bbf197d
edgarbs1998 <edgarbs1998@users.noreply.github.com>
2019-06-28 14:09:29 +0100

    Formatted code

--- goInfo_windows.go
+++ goInfo_windows.go
@@ -11,24 +11,24 @@ import (
 
 func GetInfo() *GoInfoObject {
 	out := _getInfo()
-	osStr := strings.Replace(out,"\n","",-1)
-	osStr = strings.Replace(osStr,"\r\n","",-1)
-	tmp1 := strings.Index(osStr,"[Version")
-	tmp2 := strings.Index(osStr,"]")
+	osStr := strings.Replace(out, "\n", "", -1)
+	osStr = strings.Replace(osStr, "\r\n", "", -1)
+	tmp1 := strings.Index(osStr, "[Version")
+	tmp2 := strings.Index(osStr, "]")
 	var osInfo [2]string
-	osInfo[0] = osStr[0:tmp1-1]
+	osInfo[0] = osStr[0 : tmp1-1]
 	if tmp1 == -1 || tmp2 == -1 {
 		osInfo[1] = "unknown"
 	} else {
-		osInfo[1] = osStr[tmp1+9:tmp2]
+		osInfo[1] = osStr[tmp1+9 : tmp2]
 	}
-	gio := &GoInfoObject{Kernel:"Windows",Core:osInfo[1],Platform:runtime.GOARCH,OS:osInfo[0],GoOS:runtime.GOOS,CPUs:runtime.NumCPU()}
-	gio.Hostname,_ = os.Hostname()
+	gio := &GoInfoObject{Kernel: "Windows", Core: osInfo[1], Platform: runtime.GOARCH, OS: osInfo[0], GoOS: runtime.GOOS, CPUs: runtime.NumCPU()}
+	gio.Hostname, _ = os.Hostname()
 	return gio
 }
 
 func _getInfo() string {
-	cmd := exec.Command("cmd","ver")
+	cmd := exec.Command("cmd", "ver")
 	cmd.Stdin = strings.NewReader("some input")
 	var out bytes.Buffer
 	var stderr bytes.Buffer
@@ -36,7 +36,7 @@ func _getInfo() string {
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("getInfo:",err)
+		fmt.Println("getInfo:", err)
 	}
 	return out.String()
 }
